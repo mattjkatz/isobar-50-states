9ad04108cdaa8d163b0051e9534338780452762f
mattjkatz <matthewkatz7@gmail.com>
2022-08-01 16:54:12 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data);
+    };
+
+    getUsers();
+  }, []);
+  return (
+    <div className="App">
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h1>{user.age}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
