1a48c24d1290fe5ccc14a5f52c45cfe0f754c7a8
mattjkatz <matthewkatz7@gmail.com>
2022-08-03 23:58:34 -0500

    working on a home screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import States from "./pages/States";
 import Guess from "./pages/Guess";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/states" element={<States />} />
         <Route path="/guess" element={<Guess />} />
       </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>I am the Home page!</h1>
+      <h1>I am the Home page!</h1>
+      <h1>I am the Home page!</h1>
+      <h1>I am the Home page!</h1>
+      <h1>I am the Home page!</h1>
+      <h1>I am the Home page!</h1>
+      <h1>I am the Home page!</h1>
+    </div>
+  );
+}
+
+export default Home;
