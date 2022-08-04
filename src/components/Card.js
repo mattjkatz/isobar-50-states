16e2ae285c45949815f918731e9629cbc3d1b4f1
mattjkatz <matthewkatz7@gmail.com>
2022-08-04 18:41:41 -0500

    about to change the modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Card.css";
-import { Modal } from "./Modal";
+import Modal from "./Modal";
 
 function Card(props) {
   // Modal function
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,11 @@
 import "./Modal.css";
 
-export const Modal = ({ showModal, setShowModal }) => {
-  return showModal ? (
+function Modal(props) {
+  return (
     <div className="background">
       <div className="modal-wrapper">Modal</div>
     </div>
-  ) : (
-    <div></div>
   );
-};
+}
+
+export default Modal;
