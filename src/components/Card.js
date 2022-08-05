9936e7abc5c847b21639cd2d28a7707e2321656a
mattjkatz <matthewkatz7@gmail.com>
2022-08-04 21:13:51 -0500

    working modal heck yeah

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
 import "./Card.css";
-import Modal from "./Modal";
+import Modal from "react-modal";
+// import Modal from "./Modal";
 
 function Card(props) {
-  // Modal function
-  const [showModal, setShowModal] = useState(false);
-
-  const openModal = () => {
-    setShowModal((prev) => !prev);
-  };
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <div>
-      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <Modal isOpen={modalIsOpen}>
+        <h2>{props.name}</h2>
+        <p>Body</p>
+        <button className="card-button" onClick={() => setModalIsOpen(false)}>
+          Close
+        </button>
+      </Modal>
+      {/* <div setOpenModal={setModalOpen ? 1 : 0} className="background">
+        <div className="modal-wrapper">Modal</div>
+      </div> */}
       <div className="card">
         <div className="card-body">
           <img src={props.url} alt="flag" className="card-image" />
           <h2 className="card-title">{props.name}</h2>
         </div>
-        <button className="card-button" onClick={openModal}>
+        <button className="card-button" onClick={() => setModalIsOpen(true)}>
           State Info
         </button>
       </div>
