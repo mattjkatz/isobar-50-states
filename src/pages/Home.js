62f39605aab94d1b40af15a73630f8583ebedf6b
mattjkatz <matthewkatz7@gmail.com>
2022-08-10 19:09:22 -0500

    add buttons to the home screen to take you to the states and guesses pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,18 @@ function Home() {
   return (
     <div className="home">
       <h1>I am the Home page!</h1>
+      <div className="card-grid">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="card-title">States</h2>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h2 className="card-title">Guess</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
