bcae8139d2a6fcd2bfadebfbcf1dfe333291728a
mattjkatz <matthewkatz7@gmail.com>
2022-08-13 17:20:08 -0500

    move the active sidebar to be in front of everything else

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navbar className="navbar" />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/states" element={<States />} />
